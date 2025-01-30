ad84936f6e3aa3bfe4343ae26192dbb2d2182c13
ahmedmohamedcod <ahmedcodteng1@gmail.com>
2025-01-30 12:53:43 +0200

    Add project files.

---
+++ Assignment/ArrayQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class ArrayQuery
+    {
+        private int[] numbers;
+
+        public ArrayQuery(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public int CountGreaterThan(int x)
+        {
+            return numbers.Count(num => num > x);
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0], q = input[1];
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            ArrayQuery queryHandler = new ArrayQuery(arr);
+
+            for (int i = 0; i < q; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine(queryHandler.CountGreaterThan(x));
+            }
+            #endregion
+
+
+        }
+    }
+}
